a901f5f384da565491c952d217e9ff4e030f3e1f
Makoyy07 <>
2025-02-13 22:10:06 +0800

    Last Sync: 2025-02-13 14:10:06 (Mobile)

--- if_statement_basic.py
+++ if_statement_basic.py
@@ -1,7 +1,8 @@
-temperature <= 35  # Or any other number
+temperature = 35  # Or any other number
 if temperature >= 30:
 	 
 	    
 	       
 	          
+ 
  print("It's a hot day!")  # Indented 4 spaces
